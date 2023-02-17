16353139b583f69761e00dd3e8c2eb55e32d6628
ErickLimaS <ericklima63@gmail.com>
2023-02-17 15:27:45 -0300

    SignUp Page Confirm Password added

--- front_end/pages/signup/index.tsx
+++ front_end/pages/signup/index.tsx
@@ -1,14 +1,49 @@
 import { signUpUser } from '@/api/server'
 import AlertMessage from '@/components/AlertMessage'
 import CustomDocumentHead from '@/components/CustomDocumentHead'
-import React, { FormEvent, useEffect, useState } from 'react'
+import React, { ButtonHTMLAttributes, FormEvent, InputHTMLAttributes, useRef, useState } from 'react'
 import Styles from './SignupStyles.module.css'
+import Spinner from '../../public/img/icons/Spinner1S200Px'
+import Link from 'next/link'
 
 function Signup() {
+
+    const [loading, setLoading] = useState<Boolean>(false)
+    const [passwordMatch, setPasswordMatch] = useState<Boolean>(true)
+
+    const password = useRef<null | HTMLInputElement>(null)
+    const confirmPassword = useRef<null | HTMLInputElement>(null)
+
+    // check passwords, if doest match, wont let the form to be submitted
+    function checkPassword() {
+
+        if (password.current!.value !== confirmPassword.current!.value) {
+
+            document.querySelector('input[name="confirm_password"]')!
+                .setAttribute('style', 'border-color: var(--red); background: #ad36321c');
+
+            (document.querySelector('button[name="submit"]') as HTMLButtonElement)!.disabled = true
+
+            setPasswordMatch(false)
+
+        }
+        else {
+            document.querySelector('input[name="confirm_password"]')!
+                .setAttribute('style', 'border-color: var(--black-50); background: initial');
+
+
+            (document.querySelector('button[name="submit"]') as HTMLButtonElement)!.disabled = false;
+
+            setPasswordMatch(true)
+        }
+
+    }
 
     async function submitHandler(e: FormEvent) {
 
         e.preventDefault()
+
+        setLoading(true)
 
         const form = e.target as HTMLFormElement
 
@@ -40,6 +75,8 @@ function Signup() {
 
         }
 
+        setLoading(false)
+
     }
 
     return (
@@ -56,6 +93,9 @@ function Signup() {
                         <h1 id={Styles.heading}>Cadastro</h1>
 
                         <p>Junte-se a mais de 1 milhão* de usuários!</p>
+
+
+                        <Link href="/login">Já tem uma conta? Clique aqui!</Link>
 
                     </div>
 
@@ -82,6 +122,21 @@ function Signup() {
                                 Senha
                                 <input type="password" placeholder='senha'
                                     name='password'
+                                    ref={password}
+                                    onChange={() => checkPassword()}
+                                    title='Precisa ter 8 caracteres, com números e letras.'
+                                    pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
+                                    required
+                                ></input>
+                            </label>
+
+                            <label>
+                                Confirme a Senha
+                                <input type="password" placeholder='confirme a senha'
+                                    name='confirm_password'
+                                    ref={confirmPassword}
+                                    data-wrong-password={!passwordMatch}
+                                    onChange={() => checkPassword()}
                                     title='Precisa ter 8 caracteres, com números e letras.'
                                     pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
                                     required
@@ -93,7 +148,12 @@ function Signup() {
                                 <input type="date" name='date' required></input>
                             </label>
 
-                            <button type='submit'>CADASTRAR</button>
+                            <button type='submit' name="submit"
+                                disabled={loading ? true : false}
+                            >
+                                {loading ? <Spinner /> : 'CADASTRAR'}
+                            </button>
+
 
                         </form>
 
