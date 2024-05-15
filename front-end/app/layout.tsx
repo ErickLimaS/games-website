3b7972ed2cc94117be839379ffdaa2ea27749b2c
Erick Lima <ericklima63@gmail.com>
2024-05-15 15:00:02 -0300

    style(Font): Add Helvetica Now font
    - Add Helvetica Now font files to public/assets/fonts
    - Import and configure Helvetica Now font using next/font/local
    - Apply Helvetica

--- front-end/app/layout.tsx
+++ front-end/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Layout from "./components/layout";
@@ -10,6 +11,97 @@ export const metadata: Metadata = {
   description: "",
 };
 
+const helveticaNow = localFont({
+  src: [
+    {
+      path: '../public/assets/fonts/helveticaNow/HelveticaNowText-BlackItalic.woff2',
+      weight: '900',
+      style: 'italic',
+    },
+    {
+      path: '../public/assets/fonts/helveticaNow/HelveticaNowText-ExtBdIta.woff2',
+      weight: '800',
+      style: 'italic',
+    },
+    {
+      path: '../public/assets/fonts/helveticaNow/HelveticaNowText-Black.woff2',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: '../public/assets/fonts/helveticaNow/HelveticaNowText-BoldItalic.woff2',
+      weight: 'bold',
+      style: 'italic',
+    },
+    {
+      path: '../public/assets/fonts/helveticaNow/HelveticaNowText-Bold.woff2',
+      weight: 'bold',
+      style: 'normal',
+    },
+    {
+      path: '../public/assets/fonts/helveticaNow/HelveticaNowText-RegIta.woff2',
+      weight: 'normal',
+      style: 'italic',
+    },
+    {
+      path: '../public/assets/fonts/helveticaNow/HelveticaNowText-ExtLtIta.woff2',
+      weight: '300',
+      style: 'italic',
+    },
+    {
+      path: '../public/assets/fonts/helveticaNow/HelveticaNowText-Thin.woff2',
+      weight: '100',
+      style: 'normal',
+    },
+    {
+      path: '../public/assets/fonts/helveticaNow/HelveticaNowText-ExtraLight.woff2',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: '../public/assets/fonts/helveticaNow/HelveticaNowText-Light.woff2',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../public/assets/fonts/helveticaNow/HelveticaNowText-ExtraBold.woff2',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: '../public/assets/fonts/helveticaNow/HelveticaNowText-MediumItalic.woff2',
+      weight: '500',
+      style: 'italic',
+    },
+    {
+      path: '../public/assets/fonts/helveticaNow/HelveticaNowText-Regular.woff2',
+      weight: 'normal',
+      style: 'normal',
+    },
+    {
+      path: '../public/assets/fonts/helveticaNow/HelveticaNowText-LightItalic.woff2',
+      weight: '300',
+      style: 'italic',
+    },
+    {
+      path: '../public/assets/fonts/helveticaNow/HelveticaNowText-ThinItalic.woff2',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: '../public/assets/fonts/helveticaNow/HelveticaNowText-ExtraLight.woff2',
+      weight: '100',
+      style: 'italic',
+    },
+    {
+      path: '../public/assets/fonts/helveticaNow/HelveticaNowText-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+  ],
+  variable: "--font-helvetica-now"
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -18,14 +110,14 @@ export default function RootLayout({
   return (
     <html lang="en">
 
-      <body className={inter.className}>
+      <body className={`${helveticaNow.variable} ${inter.className}`}>
 
         <Layout>
 
           {children}
 
         </Layout>
-        
+
       </body>
 
     </html>
--- front-end/tailwind.config.ts
+++ front-end/tailwind.config.ts
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        helveticaNow: ['var(--font-helvetica-now)']
+      },
       colors: {
         'primary': '#050914',
         'secondary': '#20e87d',
